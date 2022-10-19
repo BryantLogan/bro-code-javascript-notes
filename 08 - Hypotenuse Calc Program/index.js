01dff49bb68df9a7e38d39a81295caf2be1ddc71
BryantLogan <bryant.logan1@gmail.com>
2022-10-19 16:46:56 -0700

    final index.html, index.js and notes

--- 08 - Hypotenuse Calc Program/index.js
+++ 08 - Hypotenuse Calc Program/index.js
@@ -2,6 +2,18 @@ let a;
 let b;
 let c;
 
+/*
+a = window.prompt("Enter side A");
+a = Number(a);
+
+b = window.prompt("Enter side B");
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+console.log("Side C:", c);
+*/
+
 document.getElementById("submitButton").onclick = function(){
     a = document.getElementById("aTextBox").value;
     a = Number(a);
