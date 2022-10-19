7a3a67293aedda2e7eabb213a92cb3bc3e0241da
BryantLogan <bryant.logan1@gmail.com>
2022-10-19 12:13:50 -0700

    first commit

---
+++ 2 variables.js
@@ -0,0 +1,16 @@
+//  A variable is a container for storing data
+//  A vairable behaves as if it was the value that it contains
+
+//  Two steps:
+//  1. Declaration (var, let, const)
+//  2. Assignment (= assignment operator)
+
+let firstName = "Bryant"; // string
+let age = 21; // numbers
+let student = true; //booleans
+
+age = age +1;
+
+console.log(age);
+console.log(firstName);
+console.log(student);
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("I Like pizza");
+console.log("I really like pizza!");
+
+window.alert("I REALLY LOVE PIZZA!");
