c51468bc2748f3c72539cbcef75cc9f926f104ce
BryantLogan <bryant.logan1@gmail.com>
2022-10-19 12:24:55 -0700

    created variable folder and notes in index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ console.log("I Like pizza");
 console.log("I really like pizza!");
 
 window.alert("I REALLY LOVE PIZZA!");
+
+//This is a comment
+
+/*
+
+    This
+    is
+    a
+    multiline
+    comment
+*/
