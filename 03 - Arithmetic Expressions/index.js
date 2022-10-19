b6eadd06e05758885458e3304986f60e4e925689
BryantLogan <bryant.logan1@gmail.com>
2022-10-19 12:57:51 -0700

    notes on arithmetic expressions

---
+++ 03 - Arithmetic Expressions/index.js
@@ -0,0 +1,31 @@
+/* 
+    arithmetic expression is a combination of:
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evaluated to a value
+    ex y = x + 5;
+*/
+
+/* 
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication
+    4. addition and subraction
+*/
+
+let students = 20;
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// let extraStudents = students % 2;
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+let result = 1 + 2 * (3 +4);
+console.log(result)
+console.log(students)
--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-let firstName = "Bryant"; 
-let age = 33;
-let student = true;
+let students = 20;
+students = students + 1;
 
-age = age + 1;
+let extraStudents = students % 2;
 
-console.log("Hello", firstName);
-console.log("You are", age, "years old");
-console.log("Enrolled:", student)
-
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are  " + age + " years old";
-document.getElementById("p3").innerHTML = "Enrolled: " + student;
-
-//This is a comment
-
-/*
-
-    This
-    is
-    a
-    multiline
-    comment
-*/
+console.log(students)
+console.log(extraStudents)
