11b5c9473c613812d20df310628176603ef454a0
BryantLogan <bryant.logan1@gmail.com>
2022-10-20 15:54:55 -0700

    do while loops notes

---
+++ 20 - Do While Loops/index.js
@@ -0,0 +1,19 @@
+// do while loop =  do somthing,
+//                  then check the condition,
+//                  repeat if condition is true
+
+// let userName;
+
+// while(userName == ""){
+//     userName = window.prompt("Enter you name");
+// }
+
+// console.log("Hello", userName);
+
+let userName;
+
+do{
+    userName = window.prompt("Enter you name");
+}while(userName == "")
+
+console.log("Hello", userName);
