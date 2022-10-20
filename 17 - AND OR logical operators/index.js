3aa41da576608364293e2ee6bd63f90fbca22706
BryantLogan <bryant.logan1@gmail.com>
2022-10-20 15:25:31 -0700

    and/or logical operators

---
+++ 17 - AND OR logical operators/index.js
@@ -0,0 +1,19 @@
+// Gives us the ability to check more than 1 condition concurrently
+// && AND (BOTH conditions must be true)
+// || OR (Either condition can be true)
+
+let temp = -10;
+let sunny = true;
+// if(temp > 0 && temp < 30){
+//     console.log("The weather is good");
+// }
+// else{
+//     console.log("The weather is bad!");
+// }
+
+if(temp > 0 &&  temp < 30 && sunny){
+    console.log("The weather is good");
+}
+else{
+    console.log("The weather is bad!");
+}
--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-let grade = 70;
+let temp = 15;
+let sunny = true;
 
-switch(true){
-    case grade >= 90:
-        console.log("You did great!");
-        break;
-    case grade >= 80:
-        console.log("You did good!");
-        break;
-    case grade >= 70:
-        console.log("You did okay!!");
-        break;
-    case grade >= 60:
-        console.log("You passed... barely!");
-        break;
-    case grade > 60:
-        console.log("You FAILED!");
-        break;
-    default:
-        console.log(grade, "is not a letter grade!")           
+if(temp > 0 &&  temp < 30 && sunny){
+    console.log("The weather is good");
+}
+else{
+    console.log("The weather is bad!");
 }
