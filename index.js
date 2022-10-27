bdbf9c307924e106ec4166f808e62fc0c5bfdd32
BryantLogan <bryant.logan1@gmail.com>
2022-10-27 11:18:26 -0700

    template literals notes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let name = "bryant"
+let userName = "bryant";
+let items = 3;
+let total = 75;
+
+let text =
+`Hello ${userName}<br>
+You have ${items} items in your cart<br>
+Your total is $${total}<br>`;
+
+// console.log(text);
+document.getElementById("myLabel").innerHTML = text;
