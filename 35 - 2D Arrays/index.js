ee18874eafa42adb3d6b38aa41be82c98f73c70b
BryantLogan <bryant.logan1@gmail.com>
2022-11-01 15:06:53 -0700

    2D arrays notes

--- 35 - 2D Arrays/index.js
+++ 35 - 2D Arrays/index.js
@@ -1,3 +1,4 @@
+// 2D array = An array of arrays
 
 let fruits = ["apples", "oranges", "bananas"];
 let vegetables = ["carrots", "onions", "potatoes"];
@@ -5,8 +6,15 @@ let meats = ["eggs", "chicken", "fish"];
 
 let groceryList = [fruits, vegetables, meats];
 
+// for(let list of groceryList){
+//     console.log(list)
+// }
+
+groceryList[0][0] = "mangoes"; // replaces apples with mangoes
+groceryList[2][2] = "steak"; // replaces fish with steak 
+
 for(let list of groceryList){
     for(let food of list){
-        console.log(food);
+        console.log(food)
     }
 }
