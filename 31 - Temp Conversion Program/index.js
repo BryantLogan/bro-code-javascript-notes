cbc634257163bb99114203b05a1301692646221f
BryantLogan <bryant.logan1@gmail.com>
2022-11-01 14:39:26 -0700

    temp converter notes

--- 31 - Temp Conversion Program/index.js
+++ 31 - Temp Conversion Program/index.js
@@ -1,3 +1,15 @@
+// let temp = 32;
+// temp = toFahrenheit(temp);
+// console.log(temp);
+
+// function toCelsius(temp){
+//     return (temp - 32) * (5/9);
+// }
+
+// function toFahrenheit(temp){
+//     return temp * 9 / 5 + 32;
+// }
+
 
 document.getElementById("submitButton").onclick = function(){
     let temp;
