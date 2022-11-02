2787410903fab92e8b6e74c6e8450f2cea4848a2
BryantLogan <bryant.logan1@gmail.com>
2022-11-02 11:30:54 -0700

    array.filter() notes

--- 41 - array.filter()/index.js
+++ 41 - array.filter()/index.js
@@ -1,3 +1,5 @@
+// array.filter() = creates a new array with all elements
+//                  that pass the test provided by a function
 
 let ages = [18, 16, 21, 17, 19, 90];
 let adults = ages.filter(checkAge);
