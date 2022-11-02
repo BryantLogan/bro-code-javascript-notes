4ffcb9e536a6f4096d794fb2e63f150f3f4a6aff
BryantLogan <bryant.logan1@gmail.com>
2022-11-02 15:36:25 -0700

    shuffle array notes

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-let grades = [100, 50, 90, 60, 80, 70];
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-grades.sort((x, y) =>  y - x);
+shuffle(cards);
 
-grades.forEach((element) => console.log(element));
+cards.forEach(card => console.log(card));
+
+function shuffle(array){
+    let currentIndex = array.length;
+
+    while(currentIndex != 0){
+        let randomIndex = Math.floor(Math.random() * array.length)
+        currentIndex -= 1;
+
+        let temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+
+    }
+
+    return array;
+}
