56dc442c9a766a05ff6abb9e5689bde093703b72
BryantLogan <bryant.logan1@gmail.com>
2022-11-02 15:53:53 -0700

    objects notes

--- 49 - Objects/index.js
+++ 49 - Objects/index.js
@@ -1,3 +1,7 @@
+// object = A group of properties and methods
+//          properties = what an object has
+//          methods = what an object can do
+//          use . to access properties/methods
 
 const car1 = {
     model:"Mustang",
