e8cb295334c5cf35e330bbdf6ae54823fa6a0a31
BryantLogan <bryant.logan1@gmail.com>
2022-11-02 11:38:37 -0700

    sort array of numbers notes

--- 43 - Sort an Array of Numbers/index.js
+++ 43 - Sort an Array of Numbers/index.js
@@ -4,6 +4,10 @@ let grades = [100, 50, 90, 60, 80, 70];
 grades = grades.sort(ascendingSort);
 grades.forEach(print);
 
+// function descendingSort(x, y){
+//     return y -x;
+// }
+
 function ascendingSort(x, y){
     return x - y;
 }
