ff1585b7e4168f860703e59e4ee55caaea127d74
BryantLogan <bryant.logan1@gmail.com>
2022-11-03 13:45:52 -0700

    array of objects notes

--- 58 - Array of Objects/index.js
+++ 58 - Array of Objects/index.js
@@ -19,6 +19,14 @@ const car4 = new Car("Ferrari", 2025, "white");
 
 const cars = [car1, car2, car3, car4];
 
+// console.log(cars[0].model);
+// console.log(cars[1].model);
+// console.log(cars[2].model);
+
+// cars[0].drive();
+// cars[1].drive();
+// cars[2].drive();
+
 startRace(cars);
 
 function startRace(cars){
