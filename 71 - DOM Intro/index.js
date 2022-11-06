94651194df52c1bafd6d69fe8184c44c104b874f
BryantLogan <bryant.logan1@gmail.com>
2022-11-06 12:50:25 -0800

    DOM notes

---
+++ 71 - DOM Intro/index.js
@@ -0,0 +1,13 @@
+// DOM =    Document Object Model (API)
+//          An interface for changing the content of a page
+
+// console.dir(document);
+
+// console.log(document.title);
+// console.log(document.title);
+// console.log(document.URL);
+// document.title = "Title Goes here";
+// document.location = "http://www.google.com";
+
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "Hello";
--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// import {PI, getCircumference, getArea} from "./math_util.js"
-
-import * as MathUtil from "./math_util.js"
-console.log(MathUtil.PIPI);
-
-let circumference = MathUtil.getCircumference(10);
-console.log(circumference);
-
-let area = MathUtil.getArea(10);
-console.log(area);
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "Hello";
