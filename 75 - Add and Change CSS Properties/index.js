6088239ebd1f0997f398c6b100907137982fb2ad
BryantLogan <bryant.logan1@gmail.com>
2022-11-06 14:05:15 -0800

    change css properties notes

--- 75 - Add and Change CSS Properties/index.js
+++ 75 - Add and Change CSS Properties/index.js
@@ -1,4 +1,3 @@
-
 const title = document.getElementById("myTitle");
 
 // title.style.backgroundColor = "lightblue";
@@ -8,4 +7,3 @@ title.style.color = "rgb(50, 200, 250)"
 title.style.fontFamily = "consolas";
 title.style.textAlign = "center";
 title.style.border = "2px solid";
-// title.style.display = "none" hides an element
