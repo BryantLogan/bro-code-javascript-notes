a5c634ebd313bf527f58a09f5a4da96598fcfb68
BryantLogan <bryant.logan1@gmail.com>
2022-11-06 13:35:20 -0800

    dom traversal notes

--- 73 - DOM Traversal/index.js
+++ 73 - DOM Traversal/index.js
@@ -1,3 +1,11 @@
+// .firstElementChild
+// .lastElementChild
+// .parentElement
+// .nextElementSibling
+// .previousElementSibling
+// .children[]
+// Array.from(.children)
+
 let element = document.querySelector("#fruit");
 let children = Array.from(element.children);
 
