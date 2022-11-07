c03ed5c2bc93d2a01ab4dec6b983b8f4257241b3
BryantLogan <bryant.logan1@gmail.com>
2022-11-07 13:29:23 -0800

    show/hide HTML elements notes

---
+++ 78 - Show and Hide HTML Elements/index.js
@@ -0,0 +1,12 @@
+
+const myButton = document.querySelector("#myButton");
+const myImg = document.querySelector("#myImg");
+
+myButton.addEventListener("click", () => {
+    if(myImg.style.visibility == "hidden"){
+        myImg.style.visibility = "visible";
+    }
+    else{
+        myImg.style.visibility = "hidden";
+    }
+})
